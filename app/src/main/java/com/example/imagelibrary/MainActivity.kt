3451584207037709ab6e0431b2febc614f1e3ca1
pm2am <hsrathore.code@gmail.com>
2024-06-24 18:58:08 +0530

    implement zoom image view in project

--- app/src/main/java/com/example/imagelibrary/MainActivity.kt
+++ app/src/main/java/com/example/imagelibrary/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.imagelibrary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.image_library.ZoomImageView
+import java.io.FileNotFoundException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var zoomImageView: ZoomImageView? = null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        zoomImageView = findViewById(R.id.zoom_image_view)
+        val button = findViewById<Button>(R.id.choose_image_button)
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setType("image/*")
+            startActivityForResult(intent, 202);
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            try {
+                val imageUri: Uri? = data?.data
+                val imageStream = imageUri?.let { contentResolver.openInputStream(it) }
+                val selectedImage = BitmapFactory.decodeStream(imageStream)
+                zoomImageView?.setImageBitmap(selectedImage)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
